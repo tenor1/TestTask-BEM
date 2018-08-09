6dcc7e665e6e84f1b94e1e0a8bb57a3fe5e8c613
tenor1 <tenor@ukr.net>
2018-08-09 22:42:30 +0300

    rezult 2 - with bundled page

---
+++ desktop.bundles/index/index.bemdecl.js
@@ -0,0 +1,113 @@
+exports.blocks = [
+    {
+        "name": "page"
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "meta"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "css"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "header"
+            }
+        ]
+    },
+    {
+        "name": "logo"
+    },
+    {
+        "name": "menu"
+    },
+    {
+        "name": "menu",
+        "mods": [
+            {
+                "name": "place",
+                "vals": [
+                    {
+                        "name": "top"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "menu",
+        "elems": [
+            {
+                "name": "item"
+            }
+        ]
+    },
+    {
+        "name": "advert"
+    },
+    {
+        "name": "advert",
+        "elems": [
+            {
+                "name": "image"
+            }
+        ]
+    },
+    {
+        "name": "advert",
+        "elems": [
+            {
+                "name": "propos"
+            }
+        ]
+    },
+    {
+        "name": "advert",
+        "elems": [
+            {
+                "name": "h2"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "articles"
+            }
+        ]
+    },
+    {
+        "name": "article"
+    },
+    {
+        "name": "icons-greed"
+    },
+    {
+        "name": "icons-greed",
+        "elems": [
+            {
+                "name": "icon"
+            }
+        ]
+    },
+    {
+        "name": "icons-greed",
+        "elems": [
+            {
+                "name": "caption"
+            }
+        ]
+    }
+];
---
+++ desktop.bundles/index/index.deps.js
@@ -0,0 +1,227 @@
+exports.deps = [
+    {
+        "block": "page"
+    },
+    {
+        "block": "i-bem-dom"
+    },
+    {
+        "block": "inherit"
+    },
+    {
+        "block": "jquery"
+    },
+    {
+        "block": "loader"
+    },
+    {
+        "block": "loader",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "config"
+    },
+    {
+        "block": "ua"
+    },
+    {
+        "block": "objects"
+    },
+    {
+        "block": "functions"
+    },
+    {
+        "block": "dom"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "init"
+    },
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "identify"
+    },
+    {
+        "block": "next-tick"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "collection"
+    },
+    {
+        "block": "i-bem",
+        "elem": "collection"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events",
+        "mod": "type"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events",
+        "mod": "type",
+        "val": "bem"
+    },
+    {
+        "block": "events"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "init",
+        "mod": "auto"
+    },
+    {
+        "block": "page",
+        "elem": "css"
+    },
+    {
+        "block": "page",
+        "elem": "js"
+    },
+    {
+        "block": "page",
+        "elem": "meta"
+    },
+    {
+        "block": "page",
+        "elem": "header"
+    },
+    {
+        "block": "logo"
+    },
+    {
+        "block": "menu"
+    },
+    {
+        "block": "control"
+    },
+    {
+        "block": "jquery",
+        "elem": "event"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerclick"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointernative"
+    },
+    {
+        "block": "keyboard",
+        "elem": "codes"
+    },
+    {
+        "block": "menu",
+        "mod": "focused"
+    },
+    {
+        "block": "menu",
+        "elem": "item"
+    },
+    {
+        "block": "menu",
+        "elem": "group"
+    },
+    {
+        "block": "menu",
+        "elem": "group-title"
+    },
+    {
+        "block": "menu",
+        "mod": "place"
+    },
+    {
+        "block": "menu",
+        "mod": "place",
+        "val": "top"
+    },
+    {
+        "block": "advert"
+    },
+    {
+        "block": "advert",
+        "elem": "image"
+    },
+    {
+        "block": "advert",
+        "elem": "propos"
+    },
+    {
+        "block": "advert",
+        "elem": "h2"
+    },
+    {
+        "block": "page",
+        "elem": "articles"
+    },
+    {
+        "block": "article"
+    },
+    {
+        "block": "icons-greed"
+    },
+    {
+        "block": "icons-greed",
+        "elem": "icon"
+    },
+    {
+        "block": "icons-greed",
+        "elem": "caption"
+    },
+    {
+        "block": "loader",
+        "mod": "type",
+        "val": "js"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events",
+        "mod": "type",
+        "val": "dom"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "init",
+        "mod": "auto",
+        "val": true
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointer"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerpressrelease"
+    },
+    {
+        "block": "menu",
+        "mod": "focused",
+        "val": true
+    }
+];
